600066ea9ecdbfd3f5299870c8a73eb7f4673e27
ken <block24block@gmail.com>
2022-07-23 17:46:11 +0800

    highest one bits

--- lib/vdx.rb
+++ lib/vdx.rb
@@ -1,6 +1,16 @@
 require "vdx/version"
 
 module Vdx
-  class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    def highest_one_bits(int)
+      Enumerator.new do |e|
+        loop do
+          b_len = int.bit_length
+          break if b_len == 0
+          e << b_len
+          int = int ^ (1 << (b_len - 1))
+        end
+      end
+    end
+  end
 end
